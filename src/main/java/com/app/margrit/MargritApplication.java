eff366d9d10f6d4649e8c9a2baa662266f199d50
Leonardo Bruksch <leonardokbruksch@hotmail.com>
2018-02-20 12:24:44 -0300

    initial template for margrit app

---
+++ src/main/java/com/app/margrit/MargritApplication.java
@@ -0,0 +1,12 @@
+package com.app.margrit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MargritApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MargritApplication.class, args);
+	}
+}
---
+++ src/test/java/com/app/margrit/MargritApplicationTests.java
@@ -0,0 +1,16 @@
+package com.app.margrit;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MargritApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
